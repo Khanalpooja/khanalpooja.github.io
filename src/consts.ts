07551705b5bae527719e48c1b8905393d6b4b6a2
Pooja Khanal <khanalpooja425@gmail.com>
2025-03-04 17:45:01 -0600

    Update index content

--- src/consts.ts
+++ src/consts.ts
@@ -1,7 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'The Poor Tourist';
+export const SITE_TITLE = 'Pooja Khanal';
 export const SITE_DESCRIPTION = 'Welcome to my website!';
 export const NAME = 'Pooja Khanal';
 export const SITE_IMAGE = '/me.jpg';
